d7d6109d0030562e3f34dca982894466b76203a5
nyix <rsa17826@email.vccs.edu>
2026-08-14 20:39:40 -0400

    NO MESSAGE SET

--- locations.py
+++ locations.py
@@ -58,9 +58,8 @@ def create_events(world: World) -> None:
   starcount = [1, 3, 2, 2, 1, 2, 3, 1, 2, 3, 6]
   for room in range(0, 11, 1):
     for i in range(0, starcount[room], 1):
-      print(room, i)
-      _ = world.get_region("hub").add_event(
-        location_name=f"{room} - star#{i}",
+      _ = world.get_region(f"stage{room}").add_event(
+        location_name=f"stage{room} - star {i} can be got",
         item_name="flag:starCanBeGot",
         location_type=Vex2Location,
         item_type=Vex2Item,
